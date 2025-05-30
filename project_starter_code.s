b8c5186640f52b0d4e14a668d579881088253af7
tylerandreotti <tandreotti@TylerAPC.>
2025-05-30 09:22:56 -0700

    completed NaiveMult and BinaryPartitioning

--- project_starter_code.s
+++ project_starter_code.s
@@ -17,7 +17,7 @@ main:
 	bl TestPrint
 	stop
 
-	//bl TestMult		// Uncomment to test Naive implementation first
+	bl TestMult		// Uncomment to test Naive implementation first
 	// stop 
 
 	// Test the partitioned algoirthm 
@@ -137,42 +137,66 @@ NaiveMult:
 	// ...
 	// Implement this part
 	// ...
-	LSL X4, X3, #1//dr=2d
-	SUBI SP, SP, #8
-	STUR X1, [SP, #0]
-	ADD X1, XZR, X4
-	BL InitZeros
-	LDUR X1, [SP, #0]
-	ADDI SP, SP, #8
-	ADDI X5, XZR, #0//i=0
-	ADDI X6, XZR, #0//j=0
-	iloopcheck:
-	ADDI X5, X5, #1
-	SUBS XZR, X5, X3
-	B.GT iloopend
-	jloopcheck:
-	ADDI X6, X6, #1
-	SUBS XZR, X6, X3
-	B.GT jloopend
-	LSL X7, X5, #3//i*8
-	LSL X8, X6, #3//j*8
-	ADD X9, X7, X8//(i+j)*8
-	ADD X7, X7, X1//P[i]ref
-	ADD X8, X8, X2//Q[j]ref
-	ADD X9, X9, X0//R[i+j]ref
-	ADDI X10, X9, #0//holdR[i+j]ref
-	LDUR X7, [X7, #0]
-	LDUR X8, [X8, #0]
-	LDUR X9, [X9, #0]
-	MUL X7, X7, X8
-	ADD X7, X7, X9
-	STUR X7, [X10, #0]
-	ADDI X6, X6, #1
-	B jloopcheck
-	jloopend:
-	ADDI X5, X5, #1
-	B iloopcheck
-	iloopend:
+	
+	SUBI SP, SP, #32 //alloc stack for X19-X21,LR
+	STUR X19, [SP, #0]
+	STUR X20, [SP, #8]
+	STUR X21, [SP, #16]
+	STUR LR, [SP, #24]
+	
+	ADDI X19, X0, #0 //saved passed in vals in X19-X21, d doesnt need to be saved as it will not be changed
+	ADDI X20, X1, #0 
+	ADDI X21, X2, #0	
+
+	LSL X4, X3, #1 //dr=2d
+	
+	ADDI X1, X4, #0 //InitZero takes (R,dr) and R is already in X0, this movs dr to X1
+	BL InitZeros //InitZeros func call
+
+	ADDI X5, XZR, #0 //i=0
+
+	iloopNM:
+	
+	SUBS XZR, X5, X3 //check i with d
+	B.GT iloopendNM //end loop if i>d
+
+	ADDI X6, XZR, #0 //j=0
+
+	jloopNM:
+
+	SUBS XZR, X6, X3 //check j with d
+	B.GT jloopendNM //end loop if j>d
+
+	LSL X7, X5, #3 //byte index i
+	LSL X8, X6, #3 //byte index j
+	ADD X9, X7, X8 //byte indexed i+j
+
+	ADD X10, X20, X7 //mem address P[i]
+	ADD X11, X21, X8 //mem address Q[j]
+	ADD X12, X19, X9 //mem address R[i+j]
+
+	LDUR X13, [X10, #0] //P[i] val
+	LDUR X14, [X11, #0] //Q[i] val
+	LDUR X15, [X12, #0] //R[i+j] val
+
+	MUL X16, X13, X14 //val P[i]*Q[j]
+	ADD X17, X15, X16 //val R[i+j]+P[i]*Q[j]
+	STUR X17, [X12, #0] //put above val in R[i+j] address
+
+	ADDI X6, X6, #1 //j++
+	B jloopNM
+	
+	jloopendNM:
+
+	ADDI X5, X5, #1 //i++
+	B iloopNM
+
+	LDUR X19, [SP, #0] //restore X19-X21,LR regs
+	LDUR X20, [SP, #8]
+	LDUR X21, [SP, #16]
+	LDUR LR, [SP, #24]
+	ADDI SP, SP, #32 //dealloc stack
+		
 	br lr
 
 
@@ -211,7 +235,109 @@ BinaryPartitioning:
 	// ...
 	// Implement this part
 	// ...
-	br lr
+
+	SUBI SP, SP, #80 //allocate mem on the stack to temp store reg X0 through X9 and LR
+	STUR X19, [SP, #0]
+	STUR X20, [SP, #8]
+	STUR X21, [SP, #16]
+	STUR X22, [SP, #24]
+	STUR X23, [SP, #32]
+	STUR X24, [SP, #40]
+	STUR X25, [SP, #48]
+	STUR X26, [SP, #56]
+	STUR X27, [SP, #64]
+	STUR LR, [SP, #72]
+
+	ADDI X19, X0, #0 //save passed values in calleesave regs
+	ADDI X20, X1, #0
+	ADDI X21, X2, #0
+	ADDI X22, X3, #0
+
+	LSR X23, X3, #1 // l=k/2
+	
+	SUBIS XZR, X22, #2 //check k=2 
+	B.NE elsecondBP //if k+2 continue, else branch
+	
+	LDUR X9, [X20, #0] //load C val
+	LDUR X10, [X21, #0] //load I[0] val
+	LSL X10, X10, #3 //covert I[0] val to byte index
+	ADD X24, X9, X10 // C1=C+I[0]
+	LDUR X10, [X21, #8] //load I[1] val
+	LSL X10, X10, #3 //covert I[0] val to byte index
+	ADD X25, X5, X7 // C2=C+I[1]
+	
+	ADDI X4, X24, #0 //return C1 to X4
+	B BPifend
+
+	elsecondBP:	
+	ADDI X0, X19, #0 // reassign passed vals to reg x0-x3
+	ADDI X1, X20, #0 
+	ADDI X2, X21, #0
+	ADDI X3, X23, #0 //instead of initial X3 val use val of l
+	
+	BL BinaryPartitioning //1st recursive func call
+	ADDI X24, X4, #0 //save return as C1 val
+	
+	ADDI X0, X19, #0 // reassign passed vals to reg x0-x3
+	ADDI X1, X20, #0
+	LSL X9, X23, #3 // byte index l 
+	ADD X2, X21, X9 //I+l
+	ADDI X3, X23, #0 //instead of initial X3 val use val of l
+	
+	BL BinaryPartitioning //1st recursive func call
+	ADDI X25, X4, #0 //save return as C2 val
+	
+	BPifend: //continatuion after if/else
+	
+	LDUR X9, [X21, #0]
+	LDUR X10, [X21, #8]
+	SUB X11, X10, X9 // I[1]-I[0] part of dn=l(I1-I0-1)
+	SUBI X11, X11, #1 // -1 part of above
+	MUL X26, X23, X11 //l* part of above where X26 = dn
+
+	LSL X27, X26, #1 //dr=2*dn
+
+	ADDI X0, X19, #0 //assign R, C1, C2, dn to X0-X3 to pass into NaiveMult
+	ADDI X1, X24, #0
+	ADDI X2, X25, #0
+	ADDI X3, X27, #0
+
+	BL NaiveMult //NaiveMult func call
+	
+	ADDI X28, XZR, #0 //i = 0
+	
+	loopR1C1: //loop reentrance
+	
+	SUBS XZR, X28, X27 //check for i>dr
+	B.GT loopendR1C1 //end loop if i>dr
+
+	LSL X9, X28, #3 //byte index i
+	ADD X10, X19, X9 //address R[i]
+	ADD X11, X20, X9 //address C1[i]
+	LDUR X12, [X10, #0] //val R[i]
+	STUR X12, [X11, #0] //assign val of R[i] to C1[i]
+	
+	ADDI X28, X28, #1 //i++
+	B loopR1C1 //start loop again
+
+	loopendR1C1: //end of loop 
+	
+	ADDI X4, X24, #0 //return C1 to X4
+	
+	LDUR X19, [SP, #0] //restore values from stack
+	LDUR X20, [SP, #8]
+	LDUR X21, [SP, #16]
+	LDUR X22, [SP, #24]
+	LDUR X23, [SP, #32]
+	LDUR X24, [SP, #40]
+	LDUR X25, [SP, #40]
+	LDUR X26, [SP, #48]
+	LDUR X27, [SP, #56]
+	LDUR X28, [SP, #64]
+	LDUR LR, [SP, #72]
+	ADDI SP, SP, #80 //deallocate stack mem
+
+	br lr // end of BP
 
 
 TestMult:
